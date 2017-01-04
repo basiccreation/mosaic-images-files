dc61e1f1130e26db3752224430d7927c47e12503
basiccreation <basiccreation@users.noreply.github.com>
2017-01-03 22:45:47 -0800

    added review author

--- app.js
+++ app.js
@@ -25,14 +25,16 @@
   };
 });
   
-    app.controller('ReviewController', function(){
+app.controller("ReviewController", function(){
+
     this.review = {};
-  
+
     this.addReview = function(product){
+      (this.review).createdOn = Date.now();
       product.reviews.push(this.review);
-      this.review ={};
+
+      this.review = {};
     };
-  
   });
   
   var products = [{
