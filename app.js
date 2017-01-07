8200350e65f6ad6c78f6072466491440224ca957
basiccreation <basiccreation@users.noreply.github.com>
2017-01-06 22:39:33 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module('store', ['store-directives']);
+  var app = angular.module('myStore', ['store-directives']);
 
   app.controller('StoreController', function(){
     this.products = gems;
