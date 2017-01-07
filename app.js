f0d83cf53c3521d2fda41d36cf6c9bbe137b408e
basiccreation <basiccreation@users.noreply.github.com>
2017-01-06 22:37:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,82 +1,18 @@
 (function() {
-  var app = angular.module('store', []);
-  
-  app.controller('GalleryController', function(){
-    this.current = 0;
-    
-    this.setCurrent = function(value){
-      this.current=value || 0;
-    };
-  });
+  var app = angular.module('myStore', ['store-directives']);
 
   app.controller('StoreController', function(){
-    this.products = products;
+    this.products = gems;
   });
 
-  app.controller('TabController', function(){
-  this.tab = 1;
-
-  this.setTab = function(selectedTab){
-    this.tab = selectedTab;
-  };
-
-  this.isSet = function(givenTab){
-    return this.tab === givenTab;
-  };
-});
-  
-app.controller("ReviewController", function(){
-
+  app.controller('ReviewController', function() {
     this.review = {};
 
-    this.addReview = function(product){
-      (this.review).createdOn = Date.now();
+    this.addReview = function(product) {
       product.reviews.push(this.review);
 
       this.review = {};
     };
-  });
-  
-  
-  app.directive('productDescription', function(){
-    return{
-      restrict: "E",
-      templateUrl:'product-description.html'
-    };
-  });
-  
-  
-  app.directive('productSpecs', function(){
-    return {
-      restrict:"A",
-      templateUrl:'product-specs.html'
-    };
-  });
-
-   app.directive('productTabs', function(){
-    return {
-      restrict: 'E',
-      templateUrl:"product-tabs.html",
-      controller:function() {
-                    this.tab = 1;
-                        this.isSet = function(checkTab) {return this.tab === checkTab;};
-                    this.setTab = function(setTab) {this.tab = setTab;};
-            },
-      controllerAs:'tab'
-      };
-  });
-  
-  app.directive('productGallery', function(){
-    return{
-      restrict:"E",
-      templateUrl:"product-gallery.html",
-      controller: function(){
-                                this.current = 0;
-                                this.setCurrent = function(imageNumber){this.current = imageNumber || 0;
-                  };
-                                },
-    controllerAs: 'gallery'
-      };
   });
   
   var products = [{
@@ -88,9 +24,9 @@ app.controller("ReviewController", function(){
       color: '#CCC',
       faces: 14,
       images: [
-       // "images/01.jpg",
-      //  "images/05.jpg",
-       // "images/09.jpg"
+        "images/01.jpg",
+        "images/05.jpg",
+        "images/09.jpg"
       ],
       reviews: [{
         stars: 5,
